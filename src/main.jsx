e5cdde6d3e72f60c0b43d4c83ed8617251322491
elkha <elkha@DESKTOP-7E9QC34>
2025-03-01 00:14:28 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://61a34c7c82487b187960d4d2a0fc4cbc@o4508896518275072.ingest.de.sentry.io/4508896520306768",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), sentryVitePlugin({
+    org: "jsm-lt",
+    project: "javascript-react"
+  })],
+  base: '/Apple-clone/',
+
+  build: {
+    sourcemap: true
+  }
+})
